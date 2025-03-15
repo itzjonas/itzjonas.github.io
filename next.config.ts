a1c7e921e74051852358a2fecf8d82040c37ddeb
Jason Seegmiller <itzjonas@gmail.com>
2025-03-14 22:02:39 -0600

    Oops

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   webpack: (config, { defaultLoaders }) => {
     config.module.rules.push({
